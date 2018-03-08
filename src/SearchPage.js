c86d6e0592df7b786a21449c50875eded790651f
Karsten Berlin <karsten.berlin@gmail.com>
2018-03-08 21:49:01 +0100

    rewrote SearchPage

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,65 +1,68 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 //import escapeRegExp from 'escape-string-regexp'
 import Book from './Book'
 import * as BooksAPI from './BooksAPI'
 
 
 class SearchPage extends Component {
-   static propTypes = {
-    books: PropTypes.array.isRequired,
-  }
 
   state = {
-  	query: '',
+    query: '',
     searchedBooks: []
   }
 
   // when the user enters data, the query gets updated
-  // on this query, a maximum of 20 results is retrieved
+  // a maximum of 20 results is retrieved
   // and handed over to searchedBooks
   updateQuery = (query) => {
     this.setState({ query: query })
-    BooksAPI.search(query, 20).then((books) => {
-      this.setState({ searchedBooks: books })
-    })
+    if (query.length>0) {
+      BooksAPI.search(query).then((books) => {
+        this.setState({ searchedBooks: books })
+      })
+    } else {
+      this.setState({ searchedBooks: [] })
+    }
+  //  console.log("Updated search results: ", this.state.searchedBooks, " for query: ", this.state.query)
   }
 
-
- render() {
-    const { searchedBooks } = this.props
-    const { query } = this.state
-console.log(this.state.searchedBooks)
- 	return (
+  render() {
+    const query = this.state.query;
+    const searchedBooks = this.state.searchedBooks;
+    return (
+      <div>
        <div className="search-books">
          <div className="search-books-bar">
       	   <Link className='close-search' to='/'>Close</Link>
            <div className="search-books-input-wrapper">
               <input type="text"
       				 placeholder="Search by title or author"
-      				 value={query}
-      				 onChange={(event) => this.updateQuery(event.target.value)}
+               value={query}
+               onChange={(event) => this.updateQuery(event.target.value)}
               />
             </div>
          </div>
-
-         <div className="search-books-results">
-            <ol className="books-grid">
-      			{
-                  this.state.searchedBooks.map((book) => (
-      			  <li key={book.id}>
-      				<Book
-      				  book={book}
-                    />
-      			  </li>
-                 ))
-}
-			</ol>
-         </div>
        </div>
-    )
- }
+       {searchedBooks.length > 0 && (
+          <div className="search-books-results">
+             <ol className="books-grid">
+       			 {
+               searchedBooks.map((book) => (
+       			     <li key={book.id}>
+       				     <Book
+       				       book={book}
+                   />
+       			     </li>
+               ))
+             }
+		         </ol>
+          </div>
+       )}
+     </div>
+    );
+  }
 }
 
 export default SearchPage
