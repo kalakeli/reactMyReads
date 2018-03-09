95ca330a4f46c74bb50164007692c6e751f67ebf
Karsten Berlin <karsten.berlin@gmail.com>
2018-03-09 17:03:36 +0100

    feat: change shelves

--- src/Book.js
+++ src/Book.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import * as BooksAPI from './BooksAPI'
+
 
 class Book extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+  }
+
+  setShelf = (book, e) => {
+   console.log("in setShelf on shelf " + book.shelf + " changing to " + e)
+   const newShelf = e;
+   BooksAPI.update(book, newShelf)
+  }
+
   render() {
+    // const { book, updateShelf } = this.props
     const { book } = this.props
 
   	return (
@@ -10,7 +24,7 @@ class Book extends Component {
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url('+book.imageLinks.smallThumbnail+')' }} ></div>
             <div className="book-shelf-changer">
-              <select>
+              <select value={book.shelf} onChange={(event) => this.setShelf(book, event.target.value)}  >
                 <option value="none" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
@@ -20,7 +34,9 @@ class Book extends Component {
             </div>
           </div>
           <div className="book-title">{book.title}</div>
-          {book.authors && (
+          {
+            // have to ascertain that book.authors are set before running map
+            book.authors && (
             <div className="book-authors">{book.authors.map((author) => author + "\n")}</div>
          )}
 
