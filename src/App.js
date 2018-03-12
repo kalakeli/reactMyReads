2e7c3621ca332265e7285ccdb07804b968a0f9de
Karsten Berlin <karsten.berlin@gmail.com>
2018-03-12 20:02:20 +0100

    style

--- src/App.js
+++ src/App.js
@@ -1,112 +1,33 @@
-// import React from 'react'
-// import SearchPage from './SearchPage'
-// // import ListBooks from './ListBooks'
-// import BookShelf from './BookShelf'
-// import * as BooksAPI from './BooksAPI'
-// import { Route } from 'react-router-dom'
-// import './App.css'
-// import { Link } from 'react-router-dom';
-//
-// // overarching component
-// // - depending on the URL either SearchPage or BookShelf component is shown
-// // - BookShelf contains another component to show a specific Book
-// class BooksApp extends React.Component {
-//   state = {
-//     // it's all about the books which need to be able to be passed down to
-//     // child components as props
-//     books: [],
-//     // three shelves show the current selection of books; to simplify their
-//     // usage, they are added to the state with the id found in a book
-//     // plus the title shown to the user in the UI
-//     shelves: [
-//       {
-//         id: "currentlyReading", name: "Currently Reading"
-//       },{
-//         id: "wantToRead", name: "Want to Read"
-//       },{
-//         id: "read", name: "Read"
-//       }
-//     ]
-//   }
-//
-//   // use componenDidMount to read a list of all books on the shelf
-//   // the resulting list is handed over to the state
-//   componentDidMount() {
-//     BooksAPI.getAll().then((books) => {
-//       this.setState({ books })
-//     })
-//   }
-//
-//   // to reflect the changes in the shelf also in the UI, the state of the books
-//   // need to be adjusted
-//   // - this function takes the book and the new shelf and adjusts the state
-//   // - the function is handed down to the child component ListBooks and from
-//   //   there down to the child component Book
-//   addBookToShelf = (bookToAdd, shelf) => {
-//      this.setState(state => {
-//        // filter the books array to reflect the changes
-//        const updatedState = state.books.filter(book => book.id !== bookToAdd.id);
-//
-//        return {
-//          books: [...updatedState, { ...bookToAdd, shelf }]
-//        };
-//      });
-//    };
-//
-//   render() {
-//     return (
-//       <div className="app">
-//         <Route path='/search' render={() => (
-//     		    <SearchPage/>
-//         )}/>
-//         <Route exact path='/' render={() => (
-//           <div className="list-books">
-//              <div className="list-books-title">
-//                <h1>MyReads</h1>
-//              </div>
-//              <div className="list-books-content">
-//
-//                {
-//                  this.state.shelves.map((shelf) => (
-//                    <BookShelf
-//                      shelf={shelf}
-//                      key={shelf.id}
-//                      // the books need to be filtered according to the shelf id,
-//                      // otherwise all books are shown on all shelves
-//                      books={this.state.books.filter(book => book.shelf === shelf.id)}
-//                      // function to reflect the changes in UI is also passed down
-//                      addBookToShelf={this.addBookToShelf}
-//                    />
-//                  ))
-//                }
-//
-//              </div>
-//              <div className="open-search">
-//                 <Link to='/search'>Add a book</Link>
-//              </div>
-//          </div>
-//
-//       )}/>
-//
-//      </div>
-//     )
-//   }
-// }
-//
-// export default BooksApp
-
 import React from 'react'
 import SearchPage from './SearchPage'
-import ListBooks from './ListBooks'
+import BookShelf from './BookShelf'
 import * as BooksAPI from './BooksAPI'
 import { Route } from 'react-router-dom'
 import './App.css'
+import { Link } from 'react-router-dom';
 
-
+// overarching component
+// - depending on the URL either SearchPage or BookShelf component is shown
+// - BookShelf contains another component to show a specific Book
 class BooksApp extends React.Component {
   state = {
-	  books: []
+    // it's all about the books which need to be able to be passed down to
+    // child components as props
+    books: [],
+    // three shelves show the current selection of books; to simplify their
+    // usage, they are added to the state with the id found in a book
+    // plus the title shown to the user in the UI
+    shelves: [
+      {
+        id: "currentlyReading", name: "Currently Reading"
+      },{
+        id: "wantToRead", name: "Want to Read"
+      },{
+        id: "read", name: "Read"
+      }
+    ]
   }
+
   // use componenDidMount to read a list of all books on the shelf
   // the resulting list is handed over to the state
   componentDidMount() {
@@ -135,16 +56,40 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         <Route path='/search' render={() => (
-    		    <SearchPage/>
-        )}/>
-        <Route exact path='/' render={() => (
-    		    <ListBooks
-              books={this.state.books}
+    		    <SearchPage
               addBookToShelf={this.addBookToShelf}
             />
         )}/>
+        <Route exact path='/' render={() => (
+          <div className="list-books">
+             <div className="list-books-title">
+               <h1>MyReads</h1>
+             </div>
+             <div className="list-books-content">
 
-      </div>
+               {
+                 this.state.shelves.map((shelf) => (
+                   <BookShelf
+                     shelf={shelf}
+                     key={shelf.id}
+                     // the books need to be filtered according to the shelf id,
+                     // otherwise all books are shown on all shelves
+                     books={this.state.books.filter(book => book.shelf === shelf.id)}
+                     // function to reflect the changes in UI is also passed down
+                     addBookToShelf={this.addBookToShelf}
+                   />
+                 ))
+               }
+
+             </div>
+             <div className="open-search">
+                <Link to='/search'>Add a book</Link>
+             </div>
+         </div>
+
+      )}/>
+
+     </div>
     )
   }
 }
