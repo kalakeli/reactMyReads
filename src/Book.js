54e584745567dc67a221331cc22b48cbf34d8c06
Karsten Berlin <karsten.berlin@gmail.com>
2018-03-12 21:12:18 +0100

    docs: updated the comments

--- src/Book.js
+++ src/Book.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import * as BooksAPI from './BooksAPI'
 
 
+// smallest component within the app showing a single book entity; this is used
+// in the SearchPage as well as in the BookShelf page
+// a book can be shifted between shelves by changing the select value
 class Book extends Component {
 
   constructor(props) {
@@ -17,7 +20,8 @@ class Book extends Component {
 
     // update UI with book's new shelf
     this.props.addBookToShelf(this.props.book, event.target.value);
-    // update back-end data with new shelf membership
+
+    // update back-end data with new shelf
     // -> just with this, changes in the shelves are not reflected in the UI
     // this is achieved by passing the function down starting from the App via
     // ListBooks, until it can be called here via the onChange event
@@ -41,6 +45,10 @@ class Book extends Component {
           }
 
             <div className="book-shelf-changer">
+              {
+                // books on the shelf have a certain shelf they belong to; books
+                // found in the search are set to the shelf 'none'
+              }
               <select value={this.props.book.shelf ? this.props.book.shelf : 'none'} onChange={this.handleChange}  >
                 <option value="" disabled>Move to...</option>
                 <option value="currentlyReading" >Currently Reading</option>
@@ -62,47 +70,5 @@ class Book extends Component {
     )
   }
 }
-//
-//   constructor(props) {
-//     super(props);
-//     this.state = {value: this.props.book.shelf}; // get current shelf
-//   }
-//
-//   setShelf = (book, e) => {
-//    console.log("moving book from shelf " + book.shelf + " to shelf " + e)
-//    const newShelf = e;
-//    BooksAPI.update(book, newShelf)
-//
-//   }
-//
-//   render() {
-//     const { book } = this.props
-//
-//   	return (
-//         <div className="book">
-//           <div className="book-top">
-//             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url('+book.imageLinks.smallThumbnail+')' }} ></div>
-//             <div className="book-shelf-changer">
-//               <select value={this.state.value} onChange={(event) => this.setShelf(book, event.target.value)}  >
-//                 <option value="none" disabled>Move to...</option>
-//                 <option value="currentlyReading" >Currently Reading</option>
-//                 <option value="wantToRead">Want to Read</option>
-//                 <option value="read">Read</option>
-//                 <option value="none">None</option>
-//               </select>
-//             </div>
-//           </div>
-//           <div className="book-title">{book.title}</div>
-//           {
-//             // have to ascertain that book.authors are set before running map
-//             book.authors && (
-//             <div className="book-authors">{book.authors.map((author) => author + "\n")}</div>
-//          )}
-//
-//
-//         </div>
-//     )
-//   }
-// }
 
 export default Book
